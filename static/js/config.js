21c97ec320f2ea33b895b70a23c6cd117bdc2954
unknown <robinkhan1984@gmail.com>
2021-01-18 20:52:44 -0500

    map loaded

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,1 @@
-API_KEY = "pk.eyJ1Ijoicm9iaW5raGFuMTk4NCIsImEiOiJja2lzMmR0bXcwZXllMnhxamNxcmFneGtsIn0.ycbPjsgHA2IixraQFPlPJA";
+const API_KEY = "pk.eyJ1Ijoicm9iaW5raGFuMTk4NCIsImEiOiJja2lzMmR0bXcwZXllMnhxamNxcmFneGtsIn0.ycbPjsgHA2IixraQFPlPJA";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,28 +1,33 @@
-// USA coordinates need to be corrected, currently raleigh.
-const usa = [35.7596, -79.0193];
+
+
 
 var myMap = L.map("map", {
-    center:usa,
-    zoom: 13,
+    center: [0, 0],
+    zoom: 2.2
+  });
 
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: "pk.eyJ1Ijoicm9iaW5raGFuMTk4NCIsImEiOiJja2lzMmR0bXcwZXllMnhxamNxcmFneGtsIn0.ycbPjsgHA2IixraQFPlPJA"
+}).addTo(myMap);
+
+// Grabbing our GeoJSON data..
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+d3.json(link).then((data) => {
+    console.log(data);
+    var geoJsonLayer = L.geoJSON(data)
+    geoJsonLayer.addTo(myMap);
 });
 
-var url = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
-var attribution = "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>";
-
-var tileLayer = L.tileLayer(url, {
-    attribution: attribution, 
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY,
-});
-
-tileLayer.addTo(myMap);
-
-
-
+// d3.json(link, function (json) {
+//     var alldata = json.features;
+//     console.log(alldata);
+// )};
 
 
 
